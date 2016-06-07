4b31df20a6b9229ab3ab494263e7dbd3bf71f36a
sarting <thanhphan@hotmail.com>
2016-06-07 18:58:50 -0500

    contac

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://formspree.io/thanhphan@hotmail.com",
+                url: "https://formspree.io/thanhphan@hotmail.com",
                 type: "POST",
                 data: {
                     name: name,
